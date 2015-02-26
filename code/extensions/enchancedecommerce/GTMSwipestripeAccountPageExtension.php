c7997a055de2f0d77b26c8b5a60ce539c21ba2c3
Jason Zhang <jason.zhang@internetrix.com.au>
2015-02-26 14:06:12 +1100

    Added function to push payment result (success or failed ) to DataLayer

--- code/extensions/enchancedecommerce/GTMSwipestripeAccountPageExtension.php
+++ code/extensions/enchancedecommerce/GTMSwipestripeAccountPageExtension.php
@@ -8,6 +8,75 @@
 
 class GTMSwipestripeAccountPageExtension extends DataExtension {
 	
+	public function contentcontrollerInit(AccountPage_Controller $controller){
+		
+		$paymentID 	= Session::get('PaymentResultPaymentID');
+		
+		if($paymentID){
+			Session::clear('PaymentResultPaymentID');
+			Session::save();
+			
+			$payment	= DataObject::get_by_id('Payment', $paymentID);
+			if($payment && $payment->ID){
+				$orderDO = $payment->Order();
+				if($orderDO && $orderDO->ID){
+					//check payment source
+					if($payment->Method == 'EwayRapid'){
+						$errorSource = 'Eway';
+					}elseif ($payment->Method == 'PayPalExpress'){
+						$errorSource = 'Paypal';
+					}elseif ($payment->Method == 'Cheque'){	
+						$errorSource = 'Invoice';
+					}else{
+						$errorSource = 'N/A';
+					}
+					
+					//check payment status
+					if($payment->Status == 'Success'){
+						$errorMessage 	= 'OK';
+						$errorCode 		= null;
+					}elseif($payment->Status == 'Failure'){
+						//payment failed
+						$paymentErrors = $payment->Errors();
+						if($paymentErrors && $paymentErrors->Count()){
+							//errro message found
+							$errorDO = $paymentErrors->first();
+							
+							$errorMessage 	= $errorDO->ErrorMessage;
+							$errorCode 		= $errorDO->ErrorCode;
+						}else{
+							// no errro message 
+							$errorMessage 	= $errorDO->ErrorMessage;
+							$errorCode 		= $errorDO->ErrorCode;
+						}
+					}else{
+						$errorMessage 	= 'Order Pending';
+						$errorCode 		= null;
+					}
+					
+					$data = array(
+						'event' => 'irx.newTransactionError',
+						'IRXTransactionError' => array(
+							array(
+								'orderId' 		=> $orderDO->ID,        //(optional)
+								'errorCode'		=> $errorCode,            	//(optional)
+								'errorMessage'	=> $errorMessage,            	//(required)
+								'errorSource'	=> $errorSource,
+							)
+						)
+					);
+					
+					if($payment->PaidByID){
+						$data['userID'] = $payment->PaidByID;
+					}
+					
+					$controller->insertGTMDataLayer($data);
+				}
+			}
+		}
+		
+	}
+	
 	
 	public function isPaymentJustSuccessfulGTM(){
 		
