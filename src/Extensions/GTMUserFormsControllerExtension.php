8d0ef427964a42ca3f68dd946e42157764079e58
Nivanka Fonseka <nivanka@silverstripers.com>
2019-09-10 11:26:07 +0530

    fixed the undefined function call error

--- src/Extensions/GTMUserFormsControllerExtension.php
+++ src/Extensions/GTMUserFormsControllerExtension.php
@@ -9,11 +9,19 @@
 namespace Internetrix\GoogleTagManager;
 
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\UserForms\Control\UserDefinedFormController;
 
 class GTMUserFormsControllerExtension extends DataExtension {
 	// catch form data here in case the submission isn't being saved.
 	public function updateEmailData( $emailData,  $attachments){
-        $session = $this->owner->sessionGet();
-        $session->set('GTM-capturedFields-'. $this->owner->ID, $emailData['Fields']);
+	    /* @var $owner UserDefinedFormController */
+	    $owner = $this->owner;
+	    $request = $owner->getRequest();
+	    try {
+            $session = $request->getSession();
+            $session->set('GTM-capturedFields-'. $owner->data()->ID, $emailData['Fields']);
+            $session->save($request);
+        } catch (\Exception $e) {
+        }
 	}
 }
